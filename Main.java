d7a5bdf95b161d697d9acdfa6364b46efd8217c8
Attila Juhasz <106558542+attilamjuhasz@users.noreply.github.com>
2026-09-03 17:18:50 +0000

    Finished Level 2

--- Main.java
+++ Main.java
@@ -1,9 +1,36 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
 public class Main{
 
-    
 
+    public void regexFinder(String fle){
+        
+        File myObj = new File(fle);
 
-    publid static void main(String[] args){
+        try(Scanner myReader = new Scanner(myObj)){
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                Pattern pat = Pattern.compile("[a-z]");
+                Matcher mat = pat.matcher(data);
+                while(mat.find()) {
+                    System.out.println(mat.group());
+                }
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Error");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+
+        Main m = new Main();
+        m.regexFinder("file.txt");
 
     }
 }
