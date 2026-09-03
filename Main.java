c150e13498c36411145b0b7d0560dcd5535b4bea
Attila Juhasz <106558542+attilamjuhasz@users.noreply.github.com>
2026-09-03 17:23:16 +0000

    Added comments

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+// Attila Juhasz | 9/3/2026 | A program that uses regex to find letters in a given text file
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.io.File;
@@ -7,6 +9,8 @@ import java.io.FileNotFoundException;
 public class Main{
 
 
+    // Precondition: Needs a string as an input which is the name of the txt file and the string needs to be defined
+    // Postcondition: Prints all the letters in the txt
     public void regexFinder(String fle){
         
         File myObj = new File(fle);
@@ -14,7 +18,7 @@ public class Main{
         try(Scanner myReader = new Scanner(myObj)){
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                Pattern pat = Pattern.compile("[a-z]");
+                Pattern pat = Pattern.compile("[a-z]|[A-Z]");
                 Matcher mat = pat.matcher(data);
                 while(mat.find()) {
                     System.out.println(mat.group());
